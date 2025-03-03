a212ad9b96ad12c5f8363b31cb9b3544d9403730
Sheela Khusal <sheela.khusal.08@gmail.com>
2025-03-02 22:34:28 -0300

    feat: simple containers to run project

---
+++ app/ximple_library/src/main/java/com/ximple_library/App.java
@@ -0,0 +1,14 @@
+package com.ximple_library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
